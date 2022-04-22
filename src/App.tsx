cc8892fc4013e407fa4f53d9429724f4a4054051
cjdomacena <cjdomacena@yahoo.com>
2022-04-21 21:55:22 -0400

    feat: :sparkles: Added homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
---
+++ src/pages/Home.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Button from '../components/Button';
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <main className=" container my-24 mx-auto text-white">
+      <section className="max-w-2xl mx-auto p-4">
+        <h1 className="text-6xl font-heading leading-relaxed tracking-tight font-bold">
+          {' '}
+          Passion, dedication and a lot of coffee
+        </h1>
+        <p className="font-medium leading-loose text-white/80 mt-8">
+          Hello! I'm Christian Domacena, a front-end developer, currently pursuing a bachelor degree
+          in IT. I specialize in creating websites that are both simple and interactive.
+        </p>
+        <div className="flex gap-4 mt-12">
+          <Button type="default" text="say hello!" />
+          <Button type="outline" text="download cv" />
+        </div>
+        <div className="flex gap-4 mt-12 text-white">
+          <a
+            href="https://github.com/cjdomacena"
+            className="hover:text-white/60 transition-colors"
+            rel="noopener noreferrer">
+            <AiFillGithub className="w-6 h-6" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/christian-domacena-4548231b8/"
+            className="hover:text-white/60 transition-colors"
+            rel="noopener noreferrer">
+            <AiFillLinkedin className="w-6 h-6" />
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Home
