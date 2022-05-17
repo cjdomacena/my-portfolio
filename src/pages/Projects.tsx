9a8c4f99f09d6a72fbf164eefe5fc38ce5605d98
cjdomacena <cjdomacena@yahoo.com>
2022-05-17 06:16:26 -0400

    feat: :sparkles: Added suspense

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import Card from '../components/Card';
 import { AirtableRecord } from '../interface';
 
@@ -35,7 +35,39 @@ const Projects:React.FC = () => {
       </div>
 
       <div className="grid justify-center grid-cols-[repeat(auto-fill,minmax(325px,1fr))] gap-24 my-8">
-        {records && records.map((record, index) => <Card title={record.title} description={record.description} tags={record.tags} siteLink={record.siteLink} githubLink={record.githubLink} image={record.image} key={index} />)}
+        <Suspense
+          fallback={
+            <svg
+              className="animate-spin -ml-1 mr-3 h-6 w-6 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24">
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+          }>
+          {records &&
+            records.map((record, index) => (
+              <Card
+                title={record.title}
+                description={record.description}
+                tags={record.tags}
+                siteLink={record.siteLink}
+                githubLink={record.githubLink}
+                image={record.image}
+                key={index}
+              />
+            ))}
+        </Suspense>
       </div>
     </section>
   );
