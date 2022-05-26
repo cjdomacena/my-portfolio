74a145378d45966585a6aff955e0d3f1e11880a9
cjdomacena <cjdomacena@yahoo.com>
2022-05-26 18:38:22 -0400

    feat: :package: Added framer motion

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,74 +1,80 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import Card from '../components/Card';
-import { AirtableRecord } from '../interface';
+import { motion } from "framer-motion";
+import React, {  useEffect, useState } from "react";
+import Card from "../components/Card";
+import Loading from "../components/Loading";
+import { AirtableRecord } from "../interface";
 
-const Projects:React.FC = () => {
 
-  const [records, setRecords] = useState<AirtableRecord[] | null>(null);
-  
-  const fetchRecords = async () => {
-    const req = await fetch('/.netlify/functions/get-portfolio');
-    const res = await req.json();
-    if(res) {
-      setRecords(res);
-    } else {
-      alert(res.message)
+const container = {
+  show: {
+    transition: {
+      staggerChildren: 0.1
     }
   }
+}
+
+const child = {
+  hidden: {opacity: 0},
+  show: {opacity: 1},
+}
+
+const Projects: React.FC = () => {
+  const [records, setRecords] = useState<AirtableRecord[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchRecords = async () => {
+    setLoading(true);
+    try {
+      const req = await fetch("/.netlify/functions/get-portfolio");
+      const res = await req.json();
+      if (res) {
+        setRecords(res);
+      } else {
+        alert(res.message);
+      }
+    } catch (error: any) {
+      alert(error.message || error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-      fetchRecords();
-  }, [])
-    
+    fetchRecords();
+  }, []);
+
   return (
     <section className="container mx-auto my-4 h-full">
       <div className="flex justify-between items-center">
         <h1 className="font-heading text-lg font-bold">Projects</h1>
-        {/* <select
-          className="text-white border placeholder:text-white bg-black p-1 rounded "
-          placeholder="Select">
-          <option>javascript</option>
-          <option>typescript</option>
-          <option>react</option>
-          <option>tailwindcss</option>
-        </select> */}
       </div>
 
-      <div className="grid justify-center grid-cols-[repeat(auto-fill,minmax(325px,1fr))] gap-24 my-8">
-        <Suspense
-          fallback={
-            <svg
-              className="animate-spin -ml-1 mr-3 h-6 w-6 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24">
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-          }>
+      {loading ? (
+        <div className="mt-4">
+          <Loading />
+        </div>
+      ) : (
+        <motion.div
+          className="grid justify-center grid-cols-[repeat(auto-fill,minmax(325px,1fr))] gap-24 my-8"
+          variants={container}
+          initial="hidden"
+          animate="show">
           {records &&
             records.map((record, index) => (
-              <Card
-                title={record.title}
-                description={record.description}
-                tags={record.tags}
-                siteLink={record.siteLink}
-                githubLink={record.githubLink}
-                image={record.image}
-                key={index}
-              />
+              <motion.div variants={child}>
+                <Card
+                  title={record.title}
+                  description={record.description}
+                  tags={record.tags}
+                  siteLink={record.siteLink}
+                  githubLink={record.githubLink}
+                  image={record.image}
+                  key={index}
+                />
+              </motion.div>
             ))}
-        </Suspense>
-      </div>
+        </motion.div>
+      )}
     </section>
   );
 };
